c6543997cc7749200bb71c2380dc30a09a01554e
Serhat <me.serhaterdem@gmail.com>
2023-12-07 02:27:56 +0300

    end

--- app/src/main/java/com/erdemserhat/jetpackcomposebasics/businesscard/MainActivity.kt
+++ app/src/main/java/com/erdemserhat/jetpackcomposebasics/businesscard/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.erdemserhat.jetpackcomposebasics.businesscard
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.magnifier
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,112 +21,124 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.erdemserhat.jetpackcomposebasics.R
-import com.erdemserhat.jetpackcomposebasics.businesscard.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    BusinessCard()
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
+            ) {
+
+
             }
         }
     }
 }
-
-
 @Preview(
-    showSystemUi = true,
-    showBackground = true
+    showBackground = true,
+    showSystemUi = true
 )
 @Composable
-fun CardPreview() {
-    BusinessCard()
+fun PreviewCard(){
+    Card()
 
 }
 
-
 @Composable
-fun ContactInformation(titleId: Int, imageId: Int, modifier: Modifier = Modifier) {
-    Row(
-    ) {
-        val image = painterResource(id = imageId)
-        Image(
-            painter = image,
-            contentDescription = null,
-            modifier = Modifier
-                .padding(5.dp)
-                .requiredSize(24.dp)
-        )
-
-        Text(
-            text = stringResource(id = titleId),
-            modifier = Modifier
-                .padding(5.dp)
-        )
-    }
-
-}
-
-
-@Composable
-fun BusinessCard(modifier: Modifier = Modifier) {
-    val image = painterResource(id = R.drawable.software_engineer_graphic_clipart_design_free_png       )
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-        modifier = Modifier
-            .padding(top = 150.dp)
-    ) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            modifier = modifier
-                .requiredSize(255.dp)
-        )
-
-        Text(
-            text = stringResource(id = R.string.full_name),
-            textAlign = TextAlign.Justify,
-            fontSize = 32.sp
-        )
-
-        Text(
-            text = stringResource(id = R.string.title),
-            fontSize = 20.sp
-        )
-
-
-    }
+fun Card(modifier: Modifier = Modifier){
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .padding(top = 160.dp)
+            .padding(start = 30.dp)
     ) {
+        CardPhoto()
+
+
         Column(
-            horizontalAlignment = Alignment.Start
+            verticalArrangement = Arrangement.Bottom,
+            modifier = modifier
+                .padding(top = 150.dp)
+
         ) {
-            ContactInformation(titleId = R.string.mail, imageId = R.drawable.mailicon)
-            ContactInformation(titleId = R.string.phone_number, imageId = R.drawable.baseline_local_phone_24)
-            ContactInformation(titleId = R.string.address, imageId = R.drawable.baseline_my_location_24)
-
-
+            ContactInformation(R.string.mail, R.drawable.baseline_mail_24)
+            ContactInformation(
+                contactTitleId = R.string.address,
+                contactIconId = R.drawable.baseline_my_location_24
+            )
+            ContactInformation(
+                contactTitleId = R.string.phone_number,
+                contactIconId = R.drawable.baseline_local_phone_24
+            )
         }
-
-
-
     }
+
+
+
+
+
+
+
+
 }
 
+@Composable
+fun CardPhoto(modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.software_engineer_graphic_clipart_design_free_png)
+    Image(
+        painter = image,
+        contentDescription = null,
+        modifier = modifier
+            .requiredSize(250.dp)
+    )
+
+    Text(
+        text = stringResource(id = R.string.full_name),
+        fontSize = 24.sp,
+        modifier = modifier
+    )
+
+    Text(
+        text = stringResource(id = R.string.title),
+        fontSize = 24.sp
+    )
+}
+
+@Composable
+
+fun ContactInformation(
+    contactTitleId:Int,
+    contactIconId:Int,
+    modifier: Modifier = Modifier){
+
+    val contactIcon = painterResource(id = contactIconId)
+    val contactTitle = stringResource(id = contactTitleId)
+
+    Column(
+        modifier = modifier
+            .padding(10.dp)
+    ) {
+        Row(
+            horizontalArrangement =  Arrangement.Start
+        ) {
+            Image(painter = contactIcon, contentDescription = null)
+            Text(
+                text = contactTitle,
+                modifier = modifier
+                    .padding(start = 5.dp)
+            )
+        }
+    }
 
 
 
 
+}
